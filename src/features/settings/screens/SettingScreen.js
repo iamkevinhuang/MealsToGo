c035370bd94d4238a1feb716f2f81b73ec69eccc
iamkevinhuang <kvnhuang7@gmail.com>
2021-07-26 15:34:27 +0700

    final project

--- src/features/settings/screens/SettingScreen.js
+++ src/features/settings/screens/SettingScreen.js
@@ -1,13 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useCallback } from 'react';
 import { TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native';
 import { List, Avatar } from 'react-native-paper';
+import { useFocusEffect } from '@react-navigation/native';
 
 import SafeAreaComponent from '../../../components/utils/SafeAreaComponent';
 import Text from '../../../components/typography/TextComponent';
 import SpacerComponent from '../../../components/spacer/SpacerComponent';
 
 import { AuthenticationContext } from '../../../services/authentication/authentication.context';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const SettingsItem = styled(List.Item)`
   padding: ${(props) => props.theme.space[3]};
@@ -19,12 +22,32 @@ const AvatarContainer = styled.View`
 
 const SettingScreen = ({ navigation }) => {
   const { onLogout, user } = useContext(AuthenticationContext);
+  const [photo, setPhoto] = useState(null);
+
+  const getProfilePicture = async (currentUser) => {
+    const photoUri = await AsyncStorage.getItem(`${currentUser.uid}-photo`);
+    setPhoto(photoUri);
+  };
+
+  useFocusEffect(() => {
+    useCallback(() => {
+      getProfilePicture(user);
+    }, [user]);
+  });
 
   return (
     <SafeAreaComponent>
       <AvatarContainer>
         <TouchableOpacity onPress={() => navigation.navigate('Camera')}>
-          <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+          {!photo ? (
+            <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+          ) : (
+            <Avatar.Image
+              size={180}
+              source={{ uri: photo }}
+              backgroundColor="#2182BD"
+            />
+          )}
         </TouchableOpacity>
         <SpacerComponent position="top" size="large">
           <Text variant="label">{user.email}</Text>
